b50b1b282a66d9db04aeb4baa630ebdffc4f58af
Alexander Winkowski <dereference23@outlook.com>
2026-06-26 19:49:57 +0530

    Label MiuiCamera in system_ext

--- common/SEPolicy.mk
+++ common/SEPolicy.mk
@@ -4,4 +4,5 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += vendor/xiaomi/miuicamera-veux/common/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += vendor/xiaomi/miuicamera-veux/common/sepolicy/vendor
